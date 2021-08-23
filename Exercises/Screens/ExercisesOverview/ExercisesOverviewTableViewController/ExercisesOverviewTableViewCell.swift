85b3791c244d284af5d3ce9bf06fbcc92625cb0f
Will T. Ellis <wiellis@gmail.com>
2021-08-23 08:55:18 -0400

    Nil out the model when preparing for reuse

--- Exercises/Screens/ExercisesOverview/ExercisesOverviewTableViewController/ExercisesOverviewTableViewCell.swift
+++ Exercises/Screens/ExercisesOverview/ExercisesOverviewTableViewController/ExercisesOverviewTableViewCell.swift
@@ -36,6 +36,7 @@ class ExercisesOverviewTableViewCell: UITableViewCell {
         imageTask?.cancel()
         imageTask = nil
         delegate = nil
+        model = nil
     }
 
     // MARK: - Public instance methods
